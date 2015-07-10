cfda42028a8fcd057e1091f3ebe86700d5fce0b8
railgan <sclol@hotmail.com>
2015-07-11 01:32:54 +0200

    Updated Allocate Statpoints in Player GUI with Tests

--- Assets/Scripts/Game/UI/StatButton.cs
+++ Assets/Scripts/Game/UI/StatButton.cs
@@ -11,20 +11,26 @@ public class StatButton : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		canvasButton.GetComponent<CanvasGroup>().alpha = 0;
-		canvasButton.onClick.AddListener(() =>	Application.LoadLevel("StatScreen"));
+		//canvasButton.onClick.AddListener(() =>	Application.LoadLevel("StatScreen"));
+		canvasButton.onClick.AddListener(() =>	GameInformation.AvailableStatPoints = 0); //Test sets statpoints to 0
+		canvasButton.onClick.AddListener(() =>	GameInformation.Strength += 5); // Test sets Strength +5 
+		canvasButton.onClick.AddListener(() =>	GameInformation.CalculateStats()); // Test calculates the stats
 	}
 
 
 	
 	// Update is called once per frame
 	void Update () {
-		checkButton();
 		availableStatPoints = GameInformation.AvailableStatPoints;
+		checkButton();
+
 	}
 
 	void checkButton(){
 		if(availableStatPoints > 0){
 			canvasButton.GetComponent<CanvasGroup>().alpha = 1;
+		} else { 
+			canvasButton.GetComponent<CanvasGroup>().alpha = 0;
 		}
 
 }
